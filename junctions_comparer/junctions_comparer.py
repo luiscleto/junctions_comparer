0df76ac6b0a802ef81e75e49e73cb9ce52a595b1
Luís Cleto <luisccleto@gmail.com>
2016-06-01 16:46:23 +0100

    Add test samples and split junction files by chromosome

--- junctions_comparer/junctions_comparer.py
+++ junctions_comparer/junctions_comparer.py
@@ -2,8 +2,19 @@
 
 from utils import *
 
+
+__temp_dir__ = "sj_tmp"
+
 if len(sys.argv) < 3:
     e_print('Insufficient parameters provided!')
     print('Usage:')
     print('\tpython junctions_comparer.py <sample1.bed> <sample2.bed> [<sample3.bed> ...]')
 
+
+def process_samples(file_list):
+    for f in file_list:
+        split_csv(f,__temp_dir__)
+
+if not os.path.exists(__temp_dir__):
+    os.makedirs(__temp_dir__)
+process_samples(sys.argv[1:])
--- junctions_comparer/utils.py
+++ junctions_comparer/utils.py
@@ -1,6 +1,17 @@
 from __future__ import print_function
 import sys
+import csv
+import os
+from itertools import groupby
 
 
 def e_print(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+
+def split_csv(filename, outputdir):
+    for key, rows in groupby(csv.reader(open(filename), delimiter='\t'),
+                             lambda row: row[0]):
+        with open(os.path.join(outputdir, os.path.splitext(os.path.basename(filename))[0] + "_%s.bed" % key), "w+") as output:
+            for row in rows:
+                output.write("\t".join(row) + "\n")
