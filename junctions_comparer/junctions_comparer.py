391efaa9e653d9d7ba701822908ad5c86a80a06b
Luís Cleto <luisccleto@gmail.com>
2016-06-01 18:02:33 +0100

    parse junctions

--- junctions_comparer/junctions_comparer.py
+++ junctions_comparer/junctions_comparer.py
@@ -4,17 +4,63 @@ from utils import *
 
 
 __temp_dir__ = "sj_tmp"
+__out_dir__ = "sj_out"
+__out_file__ = "results.csv"
+__out_delimiter__ = ","
 
 if len(sys.argv) < 3:
-    e_print('Insufficient parameters provided!')
+    e_print('[ERROR] Insufficient parameters provided!')
     print('Usage:')
     print('\tpython junctions_comparer.py <sample1.bed> <sample2.bed> [<sample3.bed> ...]')
 
 
+def write_chromosome_junctions_to_file(chromosome, junctions):
+    writer = csv.writer(open(os.path.join(__out_dir__, __out_file__), 'a'), delimiter=__out_delimiter__, lineterminator='\n')
+    for key, value in junctions.items():
+        row = [key]
+        row.extend(value)
+        writer.writerow(row)
+
+
+def read_junctions(samples, chromosomes):
+    for c in chromosomes:
+        chromosome_junctions = {}
+        sample_index = 0
+        for s in map(lambda sam: os.path.splitext(os.path.basename(sam))[0], samples):
+            file_name = os.path.join(__temp_dir__, s + "_" + c + ".bed")
+            with open(file_name, 'rb') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter='\t')
+                for row in csv_reader:
+                    junc_id = row[0] + "_" + row[1] + "_" + row[2]
+                    if junc_id not in chromosome_junctions.keys():
+                        chromosome_junctions[junc_id] = [0] * len(samples)
+                    chromosome_junctions[junc_id][sample_index] += 1
+            sample_index += 1
+        write_chromosome_junctions_to_file(c, chromosome_junctions)
+
+
 def process_samples(file_list):
+    # Initialize csv file
+    with open(os.path.join(__out_dir__, __out_file__), "w+") as o:
+        samples = map(lambda sam: os.path.splitext(os.path.basename(sam))[0], file_list)
+        o.write("id"+__out_delimiter__+__out_delimiter__.join(samples)+"\n")
+    # Create smaller files with chromosome-specific junctions
+    print("[INFO] Splitting BED files by chromosome...")
+    chromosomes_found = []
     for f in file_list:
-        split_csv(f,__temp_dir__)
+        chromosomes_found = split_csv(f, __temp_dir__)
+    print("[DONE]")
+    if len(chromosomes_found) == 0:
+        e_print('\t[ERROR] No chromosomes found in junction files')
+        sys.exit(-1)
+    # Per chromosome, process junctions in file and add them to .csv file
+    print("[INFO] Collecting junction information by chromossome...")
+    read_junctions(file_list, sorted(list(chromosomes_found)))
+    print("[DONE]")
+
 
 if not os.path.exists(__temp_dir__):
     os.makedirs(__temp_dir__)
-process_samples(sys.argv[1:])
+if not os.path.exists(__out_dir__):
+    os.makedirs(__out_dir__)
+process_samples(sorted(sys.argv[1:]))
--- junctions_comparer/utils.py
+++ junctions_comparer/utils.py
@@ -9,9 +9,22 @@ def e_print(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
+def represents_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
 def split_csv(filename, outputdir):
-    for key, rows in groupby(csv.reader(open(filename), delimiter='\t'),
-                             lambda row: row[0]):
-        with open(os.path.join(outputdir, os.path.splitext(os.path.basename(filename))[0] + "_%s.bed" % key), "w+") as output:
+    chromosomes_found = set()
+    for key, rows in groupby(csv.reader(open(filename), delimiter='\t'), lambda r: r[0]):
+        nk = str(key).replace("chr", "")
+        if len(nk) == 1 and represents_int(nk):
+            nk = "0" + nk
+        chromosomes_found.add(nk)
+        with open(os.path.join(outputdir, os.path.splitext(os.path.basename(filename))[0] + "_%s.bed" % nk), "w+") as output:
             for row in rows:
                 output.write("\t".join(row) + "\n")
+    return chromosomes_found
